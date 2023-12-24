e0409ac659833f5a1d392896cd8dc25122441f10
Feng Haojie <873663920@qq.com>
2023-12-24 21:01:44 +0800

    message

---
+++ build/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../bin/untitled"
+  "../bin/untitled.pdb"
+  "CMakeFiles/untitled_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/untitled_autogen.dir/ParseCache.txt"
+  "untitled_autogen"
+  "CMakeFiles/untitled.dir/main.cpp.o"
+  "CMakeFiles/untitled.dir/main.cpp.o.d"
+  "CMakeFiles/untitled.dir/mainwindow.cpp.o"
+  "CMakeFiles/untitled.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/untitled.dir/untitled_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/untitled.dir/untitled_autogen/mocs_compilation.cpp.o.d"
+  "ui_mainwindow.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
